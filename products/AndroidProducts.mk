402f01e0fc2146e5795974c68f174b7a5829be90
Will DeBerry <hillbillyhacker86@gmail.com>
2012-03-26 15:42:05 -0400

    Change files for theme

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,24 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/darkred_crespo.mk \
+    $(LOCAL_DIR)/darkred_crespo4g.mk \
+    $(LOCAL_DIR)/darkred_inc.mk \
+    $(LOCAL_DIR)/darkred_maguro.mk \
+    $(LOCAL_DIR)/darkred_captivatemtd.mk \
+    $(LOCAL_DIR)/darkred_fascinatemtd.mk \
+    $(LOCAL_DIR)/darkred_galaxysmtd.mk \
+    $(LOCAL_DIR)/darkred_otter.mk \
+    $(LOCAL_DIR)/darkred_p4.mk \
+    $(LOCAL_DIR)/darkred_p4vzw.mk \
+    $(LOCAL_DIR)/darkred_p4wifi.mk \
+    $(LOCAL_DIR)/darkred_p5wifi.mk \
+    $(LOCAL_DIR)/darkred_stingray.mk \
+    $(LOCAL_DIR)/darkred_supersonic.mk \
+    $(LOCAL_DIR)/darkred_tenderloin.mk \
+    $(LOCAL_DIR)/darkred_tf101.mk\
+    $(LOCAL_DIR)/darkred_toro.mk \
+    $(LOCAL_DIR)/darkred_vibrantmtd.mk \
+    $(LOCAL_DIR)/darkred_vivow.mk \
+    $(LOCAL_DIR)/darkred_galaxytab7c.mk \
+    $(LOCAL_DIR)/darkred_wingray.mk \
+    $(LOCAL_DIR)/darkred_p999.mk \
+    $(LOCAL_DIR)/darkred_p990.mk
