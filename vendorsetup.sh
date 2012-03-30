e1e8e641052c638cf34118fb4957cd2839eb5688
George Merlocco <george@merloc.co>
2012-03-30 19:00:50 -0400

    Edited VendorSetup.sh to include tf101 + maguro PCB Blue themes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo darkred_maguro-userdebug
+add_lunch_combo pcb-blue_maguro-userdebug
 add_lunch_combo darkred_toro-userdebug
 add_lunch_combo darkred_crespo-userdebug
 add_lunch_combo darkred_crespo4g-userdebug
@@ -17,7 +17,7 @@ add_lunch_combo darkred_p5wifi-userdebug
 add_lunch_combo darkred_stingray-userdebug
 add_lunch_combo darkred_supersonic-userdebug
 add_lunch_combo darkred_tenderloin-userdebug
-add_lunch_combo darkred_tf101-userdebug
+add_lunch_combo pcb-blue_tf101-userdebug
 add_lunch_combo darkred_vibrantmtd-userdebug
 add_lunch_combo darkred_vivow-userdebug
 add_lunch_combo darkred_wingray-userdebug
