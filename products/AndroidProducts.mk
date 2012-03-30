facf047075460ce1812058059ee6ef16fab40c80
George Merlocco <george@merloc.co>
2012-03-30 02:02:17 -0400

    Updated to include PCB edits (tf101 + maguro)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,7 +2,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/darkred_crespo.mk \
     $(LOCAL_DIR)/darkred_crespo4g.mk \
     $(LOCAL_DIR)/darkred_inc.mk \
-    $(LOCAL_DIR)/darkred_maguro.mk \
+    $(LOCAL_DIR)/pcb-blue_maguro.mk \
     $(LOCAL_DIR)/darkred_captivatemtd.mk \
     $(LOCAL_DIR)/darkred_fascinatemtd.mk \
     $(LOCAL_DIR)/darkred_galaxysmtd.mk \
@@ -14,7 +14,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/darkred_stingray.mk \
     $(LOCAL_DIR)/darkred_supersonic.mk \
     $(LOCAL_DIR)/darkred_tenderloin.mk \
-    $(LOCAL_DIR)/darkred_tf101.mk\
+    $(LOCAL_DIR)/pcb-blue_tf101.mk\
     $(LOCAL_DIR)/darkred_toro.mk \
     $(LOCAL_DIR)/darkred_vibrantmtd.mk \
     $(LOCAL_DIR)/darkred_vivow.mk \
