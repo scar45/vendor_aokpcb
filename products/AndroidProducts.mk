fa68a14368b69813f119647caa125b03fe7c7009
George Merlocco <george@merloc.co>
2012-04-22 17:38:44 -0400

    HUGE device support updated

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,24 +1,24 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/darkred_crespo.mk \
-    $(LOCAL_DIR)/darkred_crespo4g.mk \
-    $(LOCAL_DIR)/darkred_inc.mk \
+    $(LOCAL_DIR)/pcbblue_crespo.mk \
+    $(LOCAL_DIR)/pcbblue_crespo4g.mk \
+    $(LOCAL_DIR)/pcbblue_inc.mk \
     $(LOCAL_DIR)/pcbblue_maguro.mk \
-    $(LOCAL_DIR)/darkred_captivatemtd.mk \
-    $(LOCAL_DIR)/darkred_fascinatemtd.mk \
-    $(LOCAL_DIR)/darkred_galaxysmtd.mk \
-    $(LOCAL_DIR)/darkred_otter.mk \
-    $(LOCAL_DIR)/darkred_p4.mk \
-    $(LOCAL_DIR)/darkred_p4vzw.mk \
-    $(LOCAL_DIR)/darkred_p4wifi.mk \
-    $(LOCAL_DIR)/darkred_p5wifi.mk \
-    $(LOCAL_DIR)/darkred_stingray.mk \
-    $(LOCAL_DIR)/darkred_supersonic.mk \
-    $(LOCAL_DIR)/darkred_tenderloin.mk \
+    $(LOCAL_DIR)/pcbblue_captivatemtd.mk \
+    $(LOCAL_DIR)/pcbblue_fascinatemtd.mk \
+    $(LOCAL_DIR)/pcbblue_galaxysmtd.mk \
+    $(LOCAL_DIR)/pcbblue_otter.mk \
+    $(LOCAL_DIR)/pcbblue_p1c.mk \
+    $(LOCAL_DIR)/pcbblue_p4.mk \
+    $(LOCAL_DIR)/pcbblue_p4vzw.mk \
+    $(LOCAL_DIR)/pcbblue_p4wifi.mk \
+    $(LOCAL_DIR)/pcbblue_p5wifi.mk \
+    $(LOCAL_DIR)/pcbblue_stingray.mk \
+    $(LOCAL_DIR)/pcbblue_supersonic.mk \
+    $(LOCAL_DIR)/pcbblue_tenderloin.mk \
     $(LOCAL_DIR)/pcbblue_tf101.mk\
     $(LOCAL_DIR)/pcbblue_toro.mk \
-    $(LOCAL_DIR)/darkred_vibrantmtd.mk \
-    $(LOCAL_DIR)/darkred_vivow.mk \
-    $(LOCAL_DIR)/darkred_p1c.mk \
-    $(LOCAL_DIR)/darkred_wingray.mk \
-    $(LOCAL_DIR)/darkred_p999.mk \
-    $(LOCAL_DIR)/darkred_p990.mk
+    $(LOCAL_DIR)/pcbblue_vibrantmtd.mk \
+    $(LOCAL_DIR)/pcbblue_vivow.mk \
+    $(LOCAL_DIR)/pcbblue_wingray.mk \
+    $(LOCAL_DIR)/pcbblue_p999.mk \
+    $(LOCAL_DIR)/pcbblue_p990.mk
