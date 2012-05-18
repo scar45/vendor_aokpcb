c01717a099b95e88077978580d44ad9fdeec9faa
remicks <lost.junglist@gmail.com>
2012-05-18 17:10:43 -0500

    Dalvik upgrade

--- products/aokpcb_toro.mk
+++ products/aokpcb_toro.mk
@@ -1,3 +1,6 @@
+# Inherit improved dalvik vm heap method
+$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+
 # Inherit AOSP device configuration for toro.
 $(call inherit-product, device/samsung/toro/full_toro.mk)
 
